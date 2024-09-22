fed91be65a9612da3fa7667a0cf157865a50c077
arad.z <arad.z@780.team>
2024-09-22 13:59:58 +0330

    initialization

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        // apps content
+        `src/**/*.{js,ts,jsx,tsx}`,
+        // packages content
+        '../../packages/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
